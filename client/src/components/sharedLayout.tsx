0d92994b7480c4a956ed49a9559897298e3c5219
segfaulty1 <segfaulty@proton.me>
2023-01-09 09:17:15 +0100

    fix: fixed the header's positioning

--- client/src/components/sharedLayout.tsx
+++ client/src/components/sharedLayout.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {Outlet} from 'react-router-dom';
+import Bridge from '../tools/bridge';
 
-export default function signup(){
+export default function signup() {
+
+
+  const initDB = async  ()=>{
+    const res = await Bridge.create('initDB');
+    console.log(res)
+  }
 
   return (
-    <h1>signup page</h1>
-  )
+    <>
+      <header className='fixed top-0 right-0 left-0 z-10 text-white'>
+        <ul className='backdrop-blur-xl h-[3rem] flex items-center gap-3 px-5'>
+          <li className='ml-auto'>profile</li>
+          <li>login</li>
+          <li>signup</li>
+          <li><button onClick={initDB}>initDB</button></li>
+        </ul>
+      </header>
+        <Outlet/>
+      <div aria-label='footer'></div>
+    </>
+  );
 }
