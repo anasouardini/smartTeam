d23769733719c593b4641cdcac8422a727e8c253
segfaulty1 <segfaulty@proton.me>
2023-01-12 18:39:45 +0100

    feat: implemented the back-end for login. resolves #9.

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -1,24 +1,37 @@
 const Bcript = require('bcrypt');
 const UserM = require('../models/user');
 
-const login = async (req, res)=>{
-  if(!req.body?.username || !req.body?.password){
-    res.status(400).jsong({data: 'the entered information is not complete'})
+const login = async (req, res, next) => {
+  if (!req.body?.username || !req.body?.password) {
+    res.status(400).jsong({ data: 'the entered information is not complete' });
   }
 
   // check if username exists
-  const checkUserResponse = await UserM.read(req.body.username);
-  if(!checkUserResponse){throw Error('error while loging in')}
-  if(!checkUserResponse[0].length){res.status(400).json({data: 'credentials are not correct.'})}
-
-  // checking if passwords match up
-  const passwordCorrect = Bcript.compare(req.body.password, checkUserResponse[0][0].password);
-  
-  // create tokens
-  if(passwordCorrect){
-    
+  // console.log(req.body);
+  const checkUserResponse = await UserM.read({ username: req.body.username });
+  if (!checkUserResponse) {
+    return next('something went bad in the server while loggin in');
+  }
+  // console.log(checkUserResponse)
+  if (!checkUserResponse[0].length) {
+    return res.status(400).json({ data: 'credentials are not correct.' });
   }
 
-}
+  // checking if passwords match up
+  const passwordCorrect = await Bcript.compare(
+    req.body.password,
+    checkUserResponse[0][0].password
+  );
 
-module.exports = login
+  if (passwordCorrect) {
+    return res.json({ data: 'logged in successfully' });
+  }
+
+  return res.status(400).json({ data: 'credentials are not correct.' });
+
+  // create tokens
+  // if(passwordCorrect){
+  //
+};
+
+module.exports = login;
