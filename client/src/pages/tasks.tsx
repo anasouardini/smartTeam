3e2cb480cb205a582af8675721132b740175c7c4
segfaulty1 <segfaulty@proton.me>
2023-02-13 14:15:47 +0100

    enhancement: tasks page now uses the side form for editing and creating new tasks. and done resolve #18.

--- client/src/pages/tasks.tsx
+++ client/src/pages/tasks.tsx
@@ -4,8 +4,8 @@ import { useOutletContext } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import Bridge from '../tools/bridge';
 import Genid from '../tools/genid';
-import Form from '../components/form';
-import { FaPen, FaTrash } from 'react-icons/fa';
+import SideForm from '../components/sideForm';
+import { FaTrash } from 'react-icons/fa';
 import { useTable } from 'react-table';
 import FormFields from '../components/formFields';
 
@@ -16,7 +16,6 @@ type queryT = {
   refetch: () => void;
 };
 type propsT = { portfoliosListQuery: queryT; projectsListQuery: queryT };
-
 const AfterQueryPrep = (props: propsT) => {
   const { loggedInUser } = useOutletContext<{
     loggedInUser: { username: string; id: string };
@@ -24,24 +23,29 @@ const AfterQueryPrep = (props: propsT) => {
   }>();
 
   const [state, setState] = React.useState({
-    popup: { sideForm: { show: true, mode: 'create', itemIndex: 0 } },
+    popup: {
+      sideForm: { random: '', show: false, mode: 'create', itemID: '' },
+    },
   });
   const stateActions = {
-    form: {
-      show: (itemID?: number, mode?: 'edit' | 'create') => {
+    sideForm: {
+      show: (itemID: string | undefined, mode: 'edit' | 'create') => {
         const stateCpy = { ...state }; // tricking react with a shallow copy
 
         if (mode == 'edit') {
-          if (!itemID) {
+          if (itemID === undefined) {
             return console.log(
               'err: forgot to include the item id for editing'
             );
           }
-          stateCpy.popup.sideForm.mode = mode;
-          stateCpy.popup.sideForm.itemIndex = itemID;
+          stateCpy.popup.sideForm.itemID = itemID;
         }
 
         stateCpy.popup.sideForm.show = true;
+        stateCpy.popup.sideForm.random = Genid(10);
+        // console.log(state.popup.sideForm.show)
+
+        stateCpy.popup.sideForm.mode = mode;
         setState(stateCpy);
       },
       hide: () => {
@@ -82,18 +86,36 @@ const AfterQueryPrep = (props: propsT) => {
   const createNewTask = () => {
     formFieldsRef.current = FormFields('task', {
       portfolio_fk: {
+        children: [
+          [
+            portfolio_fkSelectRef.current?.value,
+            portfolio_fkSelectRef.current?.innerText,
+          ],
+          ['key2', 'value2'],
+        ],
         props: {
-          defaultValue: portfolio_fkSelectRef.current?.value,
+          defaultValue: portfolio_fkSelectRef.current?.innerText,
           readOnly: true,
         },
       },
       project_fk: {
+        children: [
+          [
+            project_fkSelectRef.current?.value,
+            project_fkSelectRef.current?.innerText,
+          ],
+          ['key2', 'value2'],
+        ],
         props: {
-          defaultValue: project_fkSelectRef.current?.value,
+          defaultValue: project_fkSelectRef.current?.innerText,
           readOnly: true,
         },
       },
       assignee_fk: {
+        children: [
+          [loggedInUser.id, loggedInUser.username],
+          ['key2', 'value2'],
+        ],
         props: {
           defaultValue: loggedInUser.id,
           readOnly: true,
@@ -106,63 +128,59 @@ const AfterQueryPrep = (props: propsT) => {
       status: 'default',
     });
 
-    stateActions.form.show();
+    stateActions.sideForm.show(undefined, 'create');
   };
 
-  const editTask = (project: { [key: string]: any }) => {
-    formFieldsRef.current = FormFields('project', {
+  const editTask = async (task: { [key: string]: any }) => {
+    formFieldsRef.current = FormFields('task', {
       portfolio_fk: {
+        children: [
+          [
+            portfolio_fkSelectRef.current?.value,
+            portfolio_fkSelectRef.current?.innerText,
+          ],
+          ['key2', 'value2'],
+        ],
         props: {
-          defaultValue: portfolio_fkSelectRef.current?.value,
+          defaultValue: portfolio_fkSelectRef.current?.innerText,
         },
       },
       project_fk: {
+        children: [
+          [
+            project_fkSelectRef.current?.value,
+            project_fkSelectRef.current?.innerText,
+          ],
+          ['key2', 'value2'],
+        ],
         props: {
-          defaultValue: project_fkSelectRef.current?.value,
+          defaultValue: project_fkSelectRef.current?.innerText,
         },
       },
-      title: { props: { defaultValue: project.title } },
-      description: { props: { defaultValue: project.description } },
-      bgColor: { props: { defaultValue: project.bgColor } },
-      dueDate: { props: { defaultValue: project.dueDate } },
-      status: { props: { defaultValue: project.status } },
-      milestone: { props: { defaultValue: project.milestone } },
-      budget: { props: { defaultValue: project.budget } },
-      expense: { props: { defaultValue: project.expense } },
+      assignee_fk: {
+        children: [
+          [loggedInUser.id, loggedInUser.username],
+          ['key2', 'value2'],
+        ],
+        props: {
+          defaultValue: loggedInUser.id,
+          readOnly: true,
+        },
+      },
+      title: { props: { defaultValue: task.title } },
+      description: { props: { defaultValue: task.description } },
+      bgColor: { props: { defaultValue: task.bgColor } },
+      dueDate: { props: { defaultValue: task.dueDate } },
+      status: { props: { defaultValue: task.status } },
     });
 
-    stateActions.form.show(project.id, 'edit');
+    stateActions.sideForm.show(task.id, 'edit');
   };
 
-  const editTaskField = async (
-    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    let value = e.target.value;
-    const name = e.target.name;
+  const removeTask = async (id: string, e:React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
 
-    if (name == 'dueDate') {
-      value += ':00';
-    }
-
-    console.log(value);
-
-    const index = state.popup.sideForm.itemIndex;
-    const currentTask = tasksQuery.data[index];
-
-    // if the value hasn't changed just abort
-    if (currentTask[name] == value) {
-      return;
-    }
-
-    const userUpdateResp = await Bridge('update', `task`, {
-      id: currentTask.id,
-      [name]: value,
-    });
-    console.log(userUpdateResp);
-  };
-
-  const removeTask = async (id: string) => {
-    const resp = await Bridge('remove', `project`, {
+    const resp = await Bridge('remove', `task`, {
       id,
     });
 
@@ -174,7 +192,7 @@ const AfterQueryPrep = (props: propsT) => {
     tasksQuery.refetch();
   };
 
-  const listFields = () => {
+  const listHeaderFields = () => {
     const fields = FormFields('task', {
       // assignee_fk:{
       //   props: {
@@ -198,117 +216,26 @@ const AfterQueryPrep = (props: propsT) => {
     });
   };
 
-  const listSideTaskFields = () => {
-    const index = state.popup.sideForm.itemIndex;
-    const currentTask = tasksQuery.data[index];
-    // console.log(currentTask);
-
-    // mysql2 misses dates by addin gone hour. hence this mess
-    // basically adding an hour to the date using string manupulation
-    // because JS handles dates poorly
-    const newDate: { [key: string]: string } = {};
-    Object.keys(currentTask).forEach((itemKey) => {
-      if (itemKey.includes('Date') || itemKey.includes('date')) {
-        // console.log(currentTask[itemKey], '--------')
-        const hourPlusOne =
-          parseInt(currentTask[itemKey].split('T')[1].split(':')[0]) + 1;
-        const item = currentTask[itemKey];
-        const firstPart = item.slice(0, item.indexOf('T') + 1);
-        const secondPart = item.slice(item.indexOf(':'));
-        const result = `${firstPart}${`0${hourPlusOne}`.slice(
-          -2
-        )}${secondPart}`;
-
-        // console.log(hourPlusOne)
-        // console.log(result)
-        newDate[itemKey] = result.split('.')[0];
-        // currentTask[itemKey] = result;
-      }
-    });
-
-    // console.log(newDate.dueDate, 'last');
-
-    return (
-      <section
-        aria-label='task info'
-        className='grow mt-[2rem] py-3 px-2 border-gray-300 border-2 rounded-md'
-      >
-        <>
-          <div className={`flex flex-col gap-6`}>
-            <label className={`max-w-max`}>
-              title:
-              <input
-                onBlur={editTaskField}
-                name='title'
-                type='text'
-                className='ml-5 px-2 py-1 border-b-2 border-b-primary'
-                defaultValue={currentTask.title}
-              />
-            </label>
-            <label className={`max-w-max`}>
-              background color:
-              <input
-                onBlur={editTaskField}
-                name='bgColor'
-                type='color'
-                className='ml-5 px-2 py-1'
-                defaultValue={currentTask.bgColor}
-              />
-            </label>
-            <label className={`max-w-max`}>
-              due date:
-              <input
-                onBlur={editTaskField}
-                name='dueDate'
-                type='datetime-local'
-                className='ml-5 px-2 py-1'
-                defaultValue={newDate.dueDate}
-              />
-            </label>
-            <label>
-              description:
-              <textarea
-                onBlur={editTaskField}
-                name='description'
-                className='mt-4 px-2 py-1 border-[1px] border-gray-300 block w-full'
-                defaultValue={currentTask.description}
-              />
-            </label>
-          </div>
-        </>
-      </section>
-    );
-  };
-
   // tables suck, falling back to DIVs
   //TODO: use grid instead of a table
-  const showTaksTale = () => {
+  const showTaksTable = () => {
     return (
-      <div aria-label='tasks table' className='text-left flex flex-col gap-2'>
-        <div className={`flex`}>
-          <div className={``}>
-            <button
-              className={`text-md text-white bg-primary rounded-md px-2 py-[2px]`}
-            >
-              New Task
-            </button>
-          </div>
-        </div>
+      <div
+        aria-label='tasks table'
+        className='mt-4 text-left flex flex-col gap-2'
+      >
         {tasksQuery.data.map((task, index) => {
           return (
-            <div className={`hover:border-primary border-[1px] rounded-md`}>
-              {tasksQuery.data.map((task, index) => {
-                return (
-                  <>
-                    <input
-                      type='text'
-                      key={index}
-                      className='px-2 py-1'
-                      defaultValue={task.title}
-                    />
-                  </>
-                );
-              })}
+            <div
+              onClick={() => editTask(task)}
+              key={index}
+              className={`px-2 py-1 cursor-pointer hover:border-primary border-[1px] 
+                        rounded-md flex justify-between items-center`}
+            >
+              <h3 key={index}>{task.title}</h3>
+              <button onClick={(e) => removeTask(task.id, e)}>
+                <FaTrash className={`text-primary`} />
+              </button>
             </div>
           );
         })}
@@ -350,7 +277,7 @@ const AfterQueryPrep = (props: propsT) => {
               )
             )}
           </select>
-          {listFields()}
+          {listHeaderFields()}
           <button className={`ml-auto bg-primary text-white rounded-md px-2`}>
             Filter
           </button>
@@ -361,21 +288,29 @@ const AfterQueryPrep = (props: propsT) => {
             aria-label='tasks list'
             className='grow mt-[2rem] py-4 flex flex-col border-gray-300 border-2 rounded-md px-2'
           >
-            {tasksQuery.status == 'success' ? showTaksTale() : <></>}
-
-            {/* new task button*/}
-            <button
-              onClick={createNewTask}
-              className={`mt-3 w-max pb-1 text-primary text-md capitalize`}
-            >
-              <span className='text-xl'>+</span> add new task
-            </button>
+            <div className={`flex`}>
+              <div className={``}>
+                <button
+                  onClick={createNewTask}
+                  className={`text-md text-white bg-primary rounded-md px-2 py-[2px]`}
+                >
+                  New Task
+                </button>
+              </div>
+            </div>
+            {tasksQuery.status == 'success' ? showTaksTable() : <></>}
           </section>
 
           {state.popup.sideForm.show &&
-          tasksQuery.status == 'success' &&
-          tasksQuery.data.length ? (
-            listSideTaskFields()
+          tasksQuery.status == 'success' ? (
+            <SideForm
+              fields={formFieldsRef.current}
+              mode={state.popup.sideForm.mode}
+              route='task'
+              refetch={tasksQuery.refetch}
+              itemID={state.popup.sideForm.itemID}
+              hideForm={stateActions.sideForm.hide}
+            />
           ) : (
             <></>
           )}
@@ -386,7 +321,7 @@ const AfterQueryPrep = (props: propsT) => {
 };
 
 // react/re-render is making it hard that is why I need to split dependent react-query calls
-export default function Projects() {
+export default function Tasks() {
   const portfoliosListQuery = useQuery('portfolio list', async () => {
     const response = await Bridge('read', `portfolio/list`);
     return response?.err == 'serverError' ? false : response.data;
