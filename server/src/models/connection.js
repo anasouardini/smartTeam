645d89eaef4ef9b06cd08b616eca0596bc3db96e
segfaulty1 <segfaulty@proton.me>
2023-04-08 22:09:53 +0000

    bug: fixed some quircks.

--- server/src/models/connection.js
+++ server/src/models/connection.js
@@ -13,31 +13,40 @@ const create = async (users) => {
   return response;
 };
 
-const read = async ({ userID }) => {
-  const query = `select cnx.*, A.username as usernameA, B.username as usernameB
+const read = async (filter) => {
+  let query = `select cnx.*, A.username as usernameA, B.username as usernameB
                 from connections cnx
                 inner join users A on cnx.userA_FK=A.id
                 inner join users B on cnx.userB_FK=B.id
-                where cnx.userA_FK=? or cnx.userB_FK=?
-              ;`;
-  const response = await Pool(query, [userID, userID]);
-  if(response.err){
-    return {err: 'error while reading connections'}
+              `;
+  let vars = [];
+  if (filter && Object.keys(filter).length) {
+    query += ' where cnx.userA_FK=? or cnx.userB_FK=?';
+    vars.push(filter.userID);
+    vars.push(filter.userID);
   }
 
-  const modifiedResp = response[0].map((connection) => {
-    return connection.userA_FK != userID
-      ? {
-          id: connection.userA_FK,
-          username: connection.usernameA,
-        }
-      : {
-          id: connection.userB_FK,
-          username: connection.usernameB,
-        };
-  });
+  const response = await Pool(query, vars);
+  if (response.err) {
+    return { err: 'error while reading connections' };
+  }
+  let modifiedResp = response[0];
 
-  // the outer array is simulating the array returned by the 
+  if (filter && Object.keys(filter).length) {
+    modifiedResp = response[0].map((connection) => {
+      return connection.userA_FK != filter.userID
+        ? {
+            id: connection.userA_FK,
+            username: connection.usernameA,
+          }
+        : {
+            id: connection.userB_FK,
+            username: connection.usernameB,
+          };
+    });
+  }
+
+  // the outer array is simulating the array returned by the
   return [modifiedResp];
 };
 
