24f918d586e0cc42f20a625c9b21795a85ce352f
segfaulty1 <segfaulty@proton.me>
2023-04-08 22:05:37 +0000

    enhancement - dx: itemsList route renames the list items for compatibility with front-end, uses post method because get is not enought anymore.

--- server/src/controllers/itemsList.js
+++ server/src/controllers/itemsList.js
@@ -7,39 +7,39 @@ const Models = {
   privileges: require('../models/privileges'),
   privilegesCategories: require('../models/privilegesCategories'),
 };
+
 const read = async (req, res, next) => {
-  const queryResp = {};
-  let commonFilterCriteria = { owner_FK: req.userID };
-  let tables = {
-    users: {},
-    connections: {userID: req.userID},
-    portfolios: commonFilterCriteria,
-    projects: commonFilterCriteria,
-    tasks: commonFilterCriteria,
-  };
-  const itemsList = Object.entries(req.query);
-  for (let i = 0; i < itemsList.length; i++) {
-    const item = itemsList[i];
+  const itemsListEntries = Object.entries(req.body.items);
+  const promiseList = {};
+  for (let i = 0; i < itemsListEntries.length; i++) {
+    const item = itemsListEntries[i];
     // console.log(item);
-    queryResp[item[0]] = (
-      await Models[item[0]].list(tables[item[0]])
-    )[0];
+    // console.log(Models[item.name].list);
+    console.log(item[1])
+    promiseList[item[0]] = Models[item[0]].list(item[1].filter);
   }
 
-  for (let i = 0; i < itemsList.length; i++) {
-    const item = itemsList[i];
-    // console.log(queryResp[item[0]])
-    if (queryResp[item[0]]?.err) {
-      return next(`err while listing ${item[0]}\n${queryResp[item[0]]?.err}`);
+  for (let i = 0; i < Object.keys(promiseList).length; i++) {
+    const itemKey = Object.keys(promiseList)[i]
+    const itemValue = await Object.values(promiseList)[i]
+
+    if (itemValue?.err) {
+      return next(`err while listing ${itemKey}\n${itemValue?.err}`);
     }
 
-    if(item[1]){
-      queryResp[item[1]] = queryResp[item[0]];
-      delete queryResp[item[0]];
+    promiseList[itemKey] = itemValue[0];
+
+    // renaming the entity
+    const entityKey = itemsListEntries[i][0];
+    const entityValue = itemsListEntries[i][1];
+    if (entityKey != entityValue.name) {
+      promiseList[entityValue.name] = itemValue[0];
+      delete promiseList[entityKey];
     }
   }
 
-  res.json({ data: queryResp });
+  // console.log(promiseList)
+  res.json({ data: promiseList });
 };
 
 module.exports = { read };
