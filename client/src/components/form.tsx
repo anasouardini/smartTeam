e638a6b1bec1fc086f3d8ba5a57b0bf30f3892b1
segfaulty1 <segfaulty@proton.me>
2023-04-16 04:49:01 +0000

    bug: fixed a problem filtering edited columns, narrowed it down to only filter when the mode is udpate.

--- client/src/components/form.tsx
+++ client/src/components/form.tsx
@@ -52,7 +52,10 @@ export default function Form(props: propsT) {
     const newData = Object.keys(props.fields).reduce(
       (acc: { [key: string]: string }, fieldKey) => {
         const tagName = props.fields[fieldKey].tagName;
-        if (fieldsRefs[tagName] !== Refs.current.originalValues[fieldKey]) {
+        if (
+          props.mode === 'create' ||
+          fieldsRefs[tagName] !== Refs.current.originalValues[fieldKey]
+        ) {
           if (customFields[tagName]) {
             // the prop name of the custom input is different from the actual field name
             // the latter is what is used the identify that value in the server
