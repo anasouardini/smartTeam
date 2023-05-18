950b658df9704a09f665f5c29456e681d75733f9
segfaulty1 <segfaulty@proton.me>
2023-05-19 00:39:16 +0100

    deployment: connected the server to remote db.

--- server/src/models/dbPool.js
+++ server/src/models/dbPool.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const pool = mysql2.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 })
 
