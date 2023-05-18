85bb8b0a1451aad65952b192ad3fa0b4d53734ef
segfaulty1 <segfaulty@proton.me>
2023-05-19 00:57:49 +0100

    deployment - fix: changed the server address to the remote one.

--- client/src/vars.ts
+++ client/src/vars.ts
@@ -1,3 +1,4 @@
-const serverAddress = 'http://127.0.0.1:2000';
+// const serverAddress = 'https://127.0.0.1:2000';
+const serverAddress = 'https://smartteam.onrender.com'
 
 export default {serverAddress}
