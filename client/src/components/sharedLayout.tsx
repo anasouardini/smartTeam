5bdf9de506aa57b152dc116573bd3526875d5d46
segfaulty1 <segfaulty@proton.me>
2023-05-27 19:58:15 +0100

    minor: changed github's link to the new one.

--- client/src/components/sharedLayout.tsx
+++ client/src/components/sharedLayout.tsx
@@ -443,7 +443,7 @@ export default function sharedLayout() {
           <span className='text-black'>
             <a href={`https://anasouardini.online`}>Anas Ouardini</a>
           </span>
-        | <a href='https://github.com/segfaulty1'>Github</a>
+        | <a href='https://github.com/anasouardini'>Github</a>
         | <a href='https://twitter.com/segfaulty1'>Twitter</a>
         | <a href='https://yesfordev.com'>Blog</a>
       </footer>
